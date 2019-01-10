5cbf004aaa3d8e6c63386ed874fd4b7063a2fba0
yoyo-yeung <31932717+yoyo-yeung@users.noreply.github.com>
2019-01-10 14:45:10 +0800

    First commit for a todo list - website

---
+++ README.md
@@ -0,0 +1,9 @@
+# Todo List
+
+This is a simple to-do list web. It allows adding, removing and completing of items using simple gesture accross different sessions. 
+
+# Open the site
+You can open and try the site by simply opening the index.html in browser. 
+
+# Library used
+JQuery and fontawesome are added to assets of the file. 
---
+++ assets/js/todo.js
@@ -0,0 +1,55 @@
+var list = [];
+
+function appendNewItem(item) {
+    let dom = $.parseHTML(`<li class='item ${item.completed?'completed' :''}'><span class="remove"><i class='far fa-trash' ></i></span> ${item.content}</li>`)[0]
+    $('ul').append(dom)
+    return dom;
+}
+
+function renewLS() {
+    let string = JSON.stringify(list.map(item => ({
+        content: item.content,
+        completed: item.completed
+    })))
+    localStorage.setItem('todo', string);
+}
+$('ul').on('click', 'li', function (event) {
+    let ind = list.findIndex((item) => item.dom === $(this)[0]);
+    if(ind===-1) return;
+    list[ind].completed = !list[ind].completed;
+    renewLS()
+    $(this).toggleClass('completed')
+})
+$('ul').on('click', '.remove', function () {
+    list = list.filter(item => item.dom !== $(this).parent()[0])
+    renewLS()
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove()
+    });
+});
+
+$('#add').on('click', function () {
+    $('input[type="text"]').fadeToggle();
+})
+$('input[type="text"]').keypress(function () {
+    if (event.which !== 13) return;
+    let dom = appendNewItem({
+        content: $(this).val(),
+        completed: false
+    })
+    list.push({
+        content: $(this).val(),
+        completed: false,
+        dom
+    })
+    renewLS()
+    $(this).val('');
+});
+
+$(document).ready(function () {
+    list = JSON.parse(localStorage.getItem('todo') || '[]').map(item => ({
+        content: item.content,
+        completed: item.completed,
+        dom: appendNewItem(item)
+    }));
+})
